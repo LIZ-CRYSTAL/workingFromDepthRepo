126e278919553a0fdfd9aa52de2a034ea1248c72
Tom Murphy <murphyt7@tcd.ie>
2017-10-28 00:03:50 +0100

    Update newGpuCommands.sh

--- newGpuCommands.sh
+++ newGpuCommands.sh
@@ -34,3 +34,6 @@ cd ~/tensorflow
 
 ./configure
 bazel build --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
+
+bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+sudo pip install /tmp/tensorflow_pkg/tensorflow
