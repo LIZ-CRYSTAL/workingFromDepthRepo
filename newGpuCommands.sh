0e02e7f5600c317a42fb5112acc6fed846a7bb5c
Tom Murphy <murphyt7@tcd.ie>
2017-10-28 00:00:26 +0100

    Create newGpuCommands.sh

---
+++ newGpuCommands.sh
@@ -0,0 +1,36 @@
+sudo apt-get install openjdk-8-jdk git python-dev python3-dev python-numpy python3-numpy python-six python3-six build-essential python-pip python3-pip python-virtualenv swig python-wheel python3-wheel libcurl3-dev libcupti-dev -y
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update
+
+wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run
+sudo sh ./cuda_8.0.61_375.26_linux-run --override --silent --toolkit
+
+# install cuDNN v6.0
+CUDNN_TAR_FILE="cudnn-8.0-linux-x64-v6.0.tgz"
+wget http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/${CUDNN_TAR_FILE}
+tar -xzvf ${CUDNN_TAR_FILE}
+sudo cp -P cuda/include/cudnn.h /usr/local/cuda-8.0/include
+sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda-8.0/lib64/
+sudo chmod a+r /usr/local/cuda-8.0/lib64/libcudnn*
+
+
+
+echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"' >> ~/.bashrc
+echo 'export CUDA_HOME=/usr/local/cuda' >> ~/.bashrc
+
+
+source ~/.bashrc
+
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+
+curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get install bazel -y
+sudo apt-get upgrade bazel
+
+git clone https://github.com/tensorflow/tensorflow
+cd ~/tensorflow
+
+./configure
+bazel build --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
