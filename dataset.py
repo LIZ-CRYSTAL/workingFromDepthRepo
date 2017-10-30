bc6be42e96cb2efc1fa57c3f64a69e151c02d342
Tom Murphy <murphyt7@tcd.ie>
2017-10-30 20:01:02 +0000

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -8,37 +8,34 @@ IMAGE_WIDTH = 304
 TARGET_HEIGHT = 55
 TARGET_WIDTH = 74
 
-class DataSet:
-    def __init__(self, batch_size):
-        self.batch_size = batch_size
 
-    def csv_inputs(self, csv_file_path):
-        filename_queue = tf.train.string_input_producer([csv_file_path], shuffle=True)
-        reader = tf.TextLineReader()
-        _, serialized_example = reader.read(filename_queue)
-        filename, depth_filename = tf.decode_csv(serialized_example, [["path"], ["annotation"]])
-        # input
-        jpg = tf.read_file(filename)
-        image = tf.image.decode_jpeg(jpg, channels=3)
-        image = tf.cast(image, tf.float32)       
-        # target
-        depth_png = tf.read_file(depth_filename)
-        depth = tf.image.decode_png(depth_png, channels=1)
-        depth = tf.cast(depth, tf.float32)
-        depth = tf.div(depth, [255.0])
-        #depth = tf.cast(depth, tf.int64)
-        # resize
-        image = tf.image.resize_images(image, (IMAGE_HEIGHT, IMAGE_WIDTH))
-        depth = tf.image.resize_images(depth, (TARGET_HEIGHT, TARGET_WIDTH))
-        invalid_depth = tf.sign(depth)
-        # generate batch
-        images, depths, invalid_depths = tf.train.batch(
-            [image, depth, invalid_depth],
-            batch_size=self.batch_size,
-            num_threads=4,
-            capacity= 50 + 3 * self.batch_size,
-        )
-        return images, depths, invalid_depths
+def csv_inputs(self, csv_file_path, batch_size):
+    filename_queue = tf.train.string_input_producer([csv_file_path], shuffle=True)
+    reader = tf.TextLineReader()
+    _, serialized_example = reader.read(filename_queue)
+    filename, depth_filename = tf.decode_csv(serialized_example, [["path"], ["annotation"]])
+    # input
+    jpg = tf.read_file(filename)
+    image = tf.image.decode_jpeg(jpg, channels=3)
+    image = tf.cast(image, tf.float32)       
+    # target
+    depth_png = tf.read_file(depth_filename)
+    depth = tf.image.decode_png(depth_png, channels=1)
+    depth = tf.cast(depth, tf.float32)
+    depth = tf.div(depth, [255.0])
+    #depth = tf.cast(depth, tf.int64)
+    # resize
+    image = tf.image.resize_images(image, (IMAGE_HEIGHT, IMAGE_WIDTH))
+    depth = tf.image.resize_images(depth, (TARGET_HEIGHT, TARGET_WIDTH))
+    invalid_depth = tf.sign(depth)
+    # generate batch
+    images, depths, invalid_depths = tf.train.batch(
+        [image, depth, invalid_depth],
+        batch_size=batch_size,
+        num_threads=4,
+        capacity= 50 + 3 * batch_size,
+    )
+    return images, depths, invalid_depths
 
 
 def output_predict(depths, images, output_dir):
