2704337984c78f1777824843b58eeb6966815672
Your Name <you@example.com>
2017-11-02 11:52:31 +0000

    fixed loading of checkpoint

--- eval.py
+++ eval.py
@@ -1,4 +1,4 @@
-eval_dir = './here'
+eval_dir = 'here'
 eval_interval_secs = 600
 import math
 import model
@@ -14,7 +14,7 @@ INITIAL_LEARNING_RATE = 0.1       # Initial learning rate.
 
 num_examples = 100 #FIXME: WHAT DOES THIS DO ?????
 
-def eval_once(saverCoarse, saverRefine, summary_writer, top_k_op, summary_op):
+def eval_once(saver, summary_writer, top_k_op, summary_op):
   """Run Eval once.
 
   Args:
@@ -25,19 +25,16 @@ def eval_once(saverCoarse, saverRefine, summary_writer, top_k_op, summary_op):
   """
   config = tf.ConfigProto(device_count = {'GPU': 0})
   with tf.Session(config=config) as sess:
-    ckptCoarse = tf.train.get_checkpoint_state('./coarse')
-    ckptRefine = tf.train.get_checkpoint_state('./refine')
-    if ckptCoarse and ckptCoarse.model_checkpoint_path and ckptRefine and ckptRefine.model_checkpoint_path :
+    ckpt = tf.train.get_checkpoint_state('refine')
+    if ckpt and ckpt.model_checkpoint_path:
       # Restores from checkpoint
-      saverCoarse.restore(sess, ckptCoarse.model_checkpoint_path)
-      saverRefine.restore(sess, ckptRefine.model_checkpoint_path)
+      
+      # Restores from checkpoint
+      saver.restore(sess, ckpt.model_checkpoint_path)
       # Assuming model_checkpoint_path looks something like:
       #   /my-favorite-path/cifar10_train/model.ckpt-0,
       # extract global_step from it.
-      global_step = ckptCoarse.model_checkpoint_path.split('/')[-1].split('-')[-1]
-      global_step2 = ckptRefine.model_checkpoint_path.split('/')[-1].split('-')[-1]
-      if not global_step2 == global_step:
-          print('DIDNT MATCH !!!!!!!!')
+      global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
     else:
       print('No checkpoint file found')
       return
@@ -73,14 +70,13 @@ def eval_once(saverCoarse, saverRefine, summary_writer, top_k_op, summary_op):
     coord.request_stop()
     coord.join(threads, stop_grace_period_secs=10)
 
-from dataset import DataSet
+from dataset import csv_inputs
 import time
 def evaluate():
   """Eval CIFAR-10 for a number of steps."""
   with tf.Graph().as_default() as g:
     # Get images and labels for CIFAR-10.
-    dataset = DataSet(8)
-    images, depths, invalid_depths = dataset.csv_inputs('test.csv')
+    images, depths, invalid_depths = csv_inputs('test.csv', 8)
     # Build a Graph that computes the logits predictions from the
     # inference model.
     keep_conv = tf.placeholder(tf.float32)
@@ -90,29 +86,10 @@ def evaluate():
     # Calculate predictions.
     top_k_op = model.loss(logits, depths, invalid_depths)
     
-    init_op = tf.global_variables_initializer()
-    
     # Session
     with tf.Session(config=tf.ConfigProto()) as sess:
-        sess.run(init_op)
-    
-        coarse_params = {}
-        refine_params = {}
-                    #clean this up
-        for variable in tf.global_variables():
-            variable_name = variable.name
-            if variable_name.find("/") < 0 or variable_name.count("/") != 1:
-                continue
-            if variable_name.find('coarse') >= 0:
-                coarse_params[variable_name] = variable
-            if variable_name.find('fine') >= 0:
-                refine_params[variable_name] = variable
-        # Restore the moving average version of the learned variables for eval.
-        #variable_averages = tf.train.ExponentialMovingAverage(
-        #    MOVING_AVERAGE_DECAY)
-        #variables_to_restore = variable_averages.variables_to_restore()
-        saverCoarse = tf.train.Saver(coarse_params)
-        saverRefine = tf.train.Saver(refine_params)
+
+        saver = tf.train.Saver()
 
         # Build the summary operation based on the TF collection of Summaries.
         summary_op = tf.summary.merge_all()
@@ -120,7 +97,7 @@ def evaluate():
         summary_writer = tf.summary.FileWriter(eval_dir, g)
 
         while True:
-          eval_once(saverCoarse, saverRefine, summary_writer, top_k_op, summary_op)
+          eval_once(saver, summary_writer, top_k_op, summary_op)
           time.sleep(eval_interval_secs)
 
 
